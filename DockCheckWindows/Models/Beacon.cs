a4da77cecc5bbddc808fc77ee18c39ea41c74f86
lucland <lucas.vsc@gmail.com>
2023-12-12 13:28:27 -0300

    Beacon model added

---
+++ DockCheckWindows/Models/Beacon.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace DockCheckWindows.Models
+{
+    public class Beacon
+    {
+        [JsonProperty("rssi")]
+        public string RSSI { get; set; }
+
+        [JsonProperty("found")]
+        public DateTime found { get; set; }
+
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        public static Beacon FromJson(string jsonData)
+        {
+            return JsonConvert.DeserializeObject<Beacon>(jsonData);
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
