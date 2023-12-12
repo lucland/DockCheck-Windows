8e041da0353c661727e817ccb8305cdf4681ef27
lucland <lucas.vsc@gmail.com>
2023-12-12 13:29:01 -0300

    Receptor model added

---
+++ DockCheckWindows/Models/Receptor.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace DockCheckWindows.Models
+{
+    public class Receptor
+    {
+        [JsonProperty("vessel")]
+        public string Vessel { get; set; }
+
+        [JsonProperty("beacons")]
+        public string[] Beacons { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        public static Receptor FromJson(string jsonData)
+        {
+            return JsonConvert.DeserializeObject<Receptor>(jsonData);
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
