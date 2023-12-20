84b16a4d1dd6d279fd7755d240323644c2c9236c
lucland <lucas.vsc@gmail.com>
2023-12-20 17:52:10 -0300

    new Pic model added

---
+++ DockCheckWindows/Models/Pic.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace DockCheckWindows.Models
+{
+    public class Pic
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+
+        [JsonProperty("picture")]
+        public string Picture { get; set; }
+
+        public static Pic FromJson(string jsonData)
+        {
+            return JsonConvert.DeserializeObject<Pic>(jsonData);
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
