ed443e3ffbc2d0c9156bc98a9bd4dddff1e988cd
lucland <lucas.vsc@gmail.com>
2023-12-22 16:56:06 -0300

    Event Repository updated

--- DockCheckWindows/Repositories/EventRepository.cs
+++ DockCheckWindows/Repositories/EventRepository.cs
@@ -1,7 +1,12 @@
 ﻿using DockCheckWindows.Models;
 using DockCheckWindows.Services;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
 
 namespace DockCheckWindows.Repositories
 {
@@ -32,6 +37,43 @@ namespace DockCheckWindows.Repositories
             return JsonConvert.DeserializeObject<Event>(response);
         }
 
-        // Additional methods for syncing events, etc., can be added here.
+        public async Task<bool> SyncEventsAsync(List<Event> events)
+        {
+            MessageBox.Show("Syncing events...");
+            try
+            {
+                if (events == null || events.Count == 0)
+                {
+                    // Handle the case where there are no events to sync.
+                    return false;
+                }
+
+                if (events.Count > 100)
+                {
+                    // Handle the case where the batch size is too large.
+                    // You might want to break it down into smaller batches here.
+                    return false;
+                }
+
+                var json = JsonConvert.SerializeObject(new { events });
+                var response = await PostAsync($"{BaseUrl}/sync", json, "Event");
+
+                if (response != null)
+                {
+                    return true; // Sync successful
+                }
+                else
+                {
+                    // Handle the case where the sync failed.
+                    return false;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                // Handle network/HTTP errors
+                // Log the exception or inform the user
+                return false;
+            }
+        }
     }
 }
