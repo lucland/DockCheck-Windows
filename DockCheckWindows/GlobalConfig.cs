8b91fa67fdc8509f8e3883f4a7e025079cd0b738
lucland <lucas.vsc@gmail.com>
2024-01-09 16:33:46 -0300

    New api common variable included into GlobalConfig

---
+++ DockCheckWindows/GlobalConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockCheckWindows
+{
+    public static class GlobalConfig
+    {
+       // public static string BaseApiUrl { get; set; } = "http://172.20.255.182:3000/api/v1";
+        public static string BaseApiUrl { get; set; } = "http://localhost:3000/api/v1";
+    }
+}
