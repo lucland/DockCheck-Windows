c12f2d289976506a4e43659639e38b198050cd6a
lucland <lucas.vsc@gmail.com>
2024-01-18 11:02:36 -0300

    ApprovedUsersResponse implemented

---
+++ DockCheckWindows/Models/ApprovedUserResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockCheckWindows.Models
+{
+    public class ApprovedUsersResponse
+    {
+        public List<string> Ids { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
