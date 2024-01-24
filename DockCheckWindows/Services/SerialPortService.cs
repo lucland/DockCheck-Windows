d813d504d83448960504897407997f770f886e0c
lucland <lucas.vsc@gmail.com>
2024-01-24 15:13:59 -0300

    Add SerialPortService class with methods for opening, closing, writing to, and reading from a serial port.

--- DockCheckWindows/Services/SerialPortService.cs
+++ DockCheckWindows/Services/SerialPortService.cs
@@ -1,12 +1,99 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO.Ports;
 using System.Threading.Tasks;
 
 namespace DockCheckWindows.Services
 {
-    internal class SerialPortService
+    public class SerialPortService
     {
+        private static SerialPort _serialPort = new SerialPort("COM5", 115200);
+        private static object _lock = new object();
+
+        public static void OpenPort()
+        {
+            lock (_lock)
+            {
+                if (!_serialPort.IsOpen)
+                {
+                    _serialPort.Open();
+                }
+            }
+        }
+
+        public static void ClosePort()
+        {
+            lock (_lock)
+            {
+                if (_serialPort.IsOpen)
+                {
+                    _serialPort.Close();
+                }
+            }
+        }
+
+        public static void WriteToPort(string data)
+        {
+            lock (_lock)
+            {
+                if (_serialPort.IsOpen)
+                {
+                    _serialPort.WriteLine(data);
+                }
+            }
+        }
+
+        public static string ReadFromPort(int timeout)
+        {
+            lock (_lock)
+            {
+                if (_serialPort.IsOpen)
+                {
+                    _serialPort.ReadTimeout = timeout;
+                    try
+                    {
+                        return _serialPort.ReadLine();
+                    }
+                    catch (TimeoutException)
+                    {
+                        return null;
+                    }
+                }
+                return null;
+            }
+        }
+
+        public static async Task<string> ReadLineWithTimeout(int timeout)
+        {
+            return await Task.Run(() =>
+            {
+                lock (_lock)
+                {
+                    if (_serialPort.IsOpen)
+                    {
+                        _serialPort.ReadTimeout = timeout;
+                        try
+                        {
+                            return _serialPort.ReadLine();
+                        }
+                        catch (TimeoutException)
+                        {
+                            return null;
+                        }
+                    }
+                    return null;
+                }
+            });
+        }
+
+        public static bool IsPortOpen
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _serialPort.IsOpen;
+                }
+            }
+        }
     }
 }
